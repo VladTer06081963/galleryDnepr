b4faa79123f1786d5b1ec52788d2a65224feed75
VladTer06081963 <patrol200611@ukr.net>
2023-11-10 16:39:45 +0200

    Gallery Dnepr

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 //------------------------------------------------------------------------
 
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const router = require('./routes');
-const session = require('express-session');
-const multer = require('multer');
-const fs = require('fs');
-const path = require('path');
-const basicAuth = require('basic-auth');
+const express = require("express");
+const router = require("./routes");
+const session = require("express-session");
+const multer = require("multer");
+const fs = require("fs");
+const path = require("path");
+const basicAuth = require("basic-auth");
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -18,15 +18,15 @@ const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD;
 // Функция для сохранения данных
 const saveData = (data) => {
   const jsonData = JSON.stringify(data);
-  fs.writeFileSync('data.json', jsonData);
+  fs.writeFileSync("data.json", jsonData);
 };
 // Функция для загрузки данных
 const loadData = () => {
   try {
-    const data = fs.readFileSync('data.json', 'utf8');
+    const data = fs.readFileSync("data.json", "utf8");
     return JSON.parse(data);
   } catch (err) {
-    console.error('Error reading the file:', err);
+    console.error("Error reading the file:", err);
     return {
       h2: [],
       p: [],
@@ -38,7 +38,7 @@ const { h2, p } = loadData();
 
 app.use(
   session({
-    secret: 'aRandomSecretKey',
+    secret: "aRandomSecretKey",
     resave: false,
     saveUninitialized: true,
   })
@@ -55,79 +55,79 @@ const authMiddleware = (req, res, next) => {
     req.session.userId = `users/${credentials.name}`;
     next();
   } else {
-    res.setHeader('WWW-Authenticate', 'Basic realm="Enter admin credentials"');
-    res.status(401).send('Authentication required');
+    res.setHeader("WWW-Authenticate", 'Basic realm="Enter admin credentials"');
+    res.status(401).send("Authentication required");
   }
 };
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json()); //для анализа application/json
-app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname, 'views'));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 const upload = multer({ storage: multer.memoryStorage() });
 
 //----------
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   const isLoggedIn = !!req.session.userId;
 
-  res.render('index', { h2, p, title: 'Tobaco', isLoggedIn });
+  res.render("index", { h2, p, title: "my Town", isLoggedIn });
 });
 
-app.get('/admin-login', (req, res) => {
-  res.render('admin-login');
+app.get("/admin-login", (req, res) => {
+  res.render("admin-login");
 });
 
-app.post('/admin-login', (req, res) => {
+app.post("/admin-login", (req, res) => {
   const { username, password } = req.body;
 
   if (username === ADMIN_USERNAME && password === ADMIN_PASSWORD) {
     req.session.userId = `users/${username}`;
-    res.redirect('/');
+    res.redirect("/");
   } else {
     // res.status(401).send('Invalid credentials. Please try again.');
-    res.redirect('/');
+    res.redirect("/");
   }
 });
 
-app.get('/logout', (req, res) => {
+app.get("/logout", (req, res) => {
   req.session.destroy();
-  res.redirect('/');
+  res.redirect("/");
 });
 
-app.post('/uploadfulls', authMiddleware, upload.single('image'), (req, res) => {
+app.post("/uploadfulls", authMiddleware, upload.single("image"), (req, res) => {
   const userId = req.session.userId;
-  const number = String(req.body.number).padStart(2, '0');
+  const number = String(req.body.number).padStart(2, "0");
   const filePath = `./public/images/fulls/${number}${path.extname(
     req.file.originalname
   )}`;
   fs.writeFileSync(filePath, req.file.buffer);
-  res.redirect('/');
+  res.redirect("/");
 });
 
 app.post(
-  '/uploadthumbs',
+  "/uploadthumbs",
   authMiddleware,
-  upload.single('image'),
+  upload.single("image"),
   (req, res) => {
     const userId = req.session.userId;
-    const number = String(req.body.number).padStart(2, '0');
+    const number = String(req.body.number).padStart(2, "0");
     const filePath = `./public/images/thumbs/${number}${path.extname(
       req.file.originalname
     )}`;
     fs.writeFileSync(filePath, req.file.buffer);
-    res.redirect('/');
+    res.redirect("/");
   }
 );
-app.post('/update', (req, res) => {
+app.post("/update", (req, res) => {
   const { articleIndex, elementType, newText } = req.body;
 
   // Обновляем нужный элемент по индексу и типу
-  if (elementType === 'h2') {
+  if (elementType === "h2") {
     h2[articleIndex] = newText;
-  } else if (elementType === 'p') {
+  } else if (elementType === "p") {
     p[articleIndex] = newText;
   }
   saveData({ h2, p });
@@ -135,7 +135,7 @@ app.post('/update', (req, res) => {
   res.json({ success: true });
 });
 
-app.get('/currentText', (req, res) => {
+app.get("/currentText", (req, res) => {
   const { articleIndex, elementType } = req.query;
   const data = loadData();
   const index = Number(articleIndex);
@@ -143,12 +143,12 @@ app.get('/currentText', (req, res) => {
   if (
     data &&
     data[elementType] &&
-    typeof data[elementType][index] !== 'undefined'
+    typeof data[elementType][index] !== "undefined"
   ) {
-    res.setHeader('Content-Type', 'application/json', { encoding: 'utf8' });
+    res.setHeader("Content-Type", "application/json", { encoding: "utf8" });
     res.json({ currentText: data[elementType][index] });
   } else {
-    res.status(404).json({ error: 'Text not found' });
+    res.status(404).json({ error: "Text not found" });
   }
 });
 
--- public/index.js
+++ public/index.js
@@ -1,9 +1,9 @@
 // JavaScript
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
   // Код для обновления текста
-  const articleIndexInput = document.getElementById('articleIndex');
-  const elementTypeInput = document.getElementById('elementType');
-  const currentTextElement = document.getElementById('currentText');
+  const articleIndexInput = document.getElementById("articleIndex");
+  const elementTypeInput = document.getElementById("elementType");
+  const currentTextElement = document.getElementById("currentText");
 
   const updateCurrentText = async () => {
     const articleIndex = articleIndexInput.value - 1; //----- поставил-1
@@ -14,29 +14,34 @@ document.addEventListener('DOMContentLoaded', function () {
       `/currentText?articleIndex=${articleIndex}&elementType=${elementType}`
     );
     const data = await response.json();
-    currentTextElement.innerText = data.currentText || 'Текст не найден';
+
+    //console.log(data);
+    currentTextElement.innerText =
+      data.currentText || "Выберите Индекс article от 1 до 12:";
+    //console.log(currentTextElement);
   };
 
-  articleIndexInput.addEventListener('change', updateCurrentText);
-  elementTypeInput.addEventListener('change', updateCurrentText);
+  articleIndexInput.addEventListener("change", updateCurrentText);
+  elementTypeInput.addEventListener("change", updateCurrentText);
 
   updateCurrentText();
 
   // Ваш исходный скрипт
-  const form = document.getElementById('updateForm');
+  const form = document.getElementById("updateForm");
 
-  form.addEventListener('submit', function (e) {
+  form.addEventListener("submit", function (e) {
     e.preventDefault();
 
     // const articleIndex = form.articleIndex.value;
+
     const articleIndex = form.articleIndex.value - 1;
     const elementType = form.elementType.value;
     const newText = form.newText.value;
 
-    fetch('/update', {
-      method: 'POST',
+    fetch("/update", {
+      method: "POST",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({ articleIndex, elementType, newText }),
     })
@@ -46,3 +51,54 @@ document.addEventListener('DOMContentLoaded', function () {
       });
   });
 });
+
+// ---------------------------------------------------------------------------
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   // Код для обновления текста
+//   const articleIndexInput = document.getElementById('articleIndex');
+//   const elementTypeInput = document.getElementById('elementType');
+//   const currentTextElement = document.getElementById('currentText');
+
+//   const updateCurrentText = async () => {
+//     const articleIndex = articleIndexInput.value - 1; //----- поставил-1
+//     const elementType = elementTypeInput.value;
+
+//     // Получаем текущий текст с сервера
+//     const response = await fetch(
+//       `/currentText?articleIndex=${articleIndex}&elementType=${elementType}`
+//     );
+
+//     const data = await response.json();
+//     currentTextElement.innerText = data.currentText || "Текст не найден";
+//   };
+
+//   articleIndexInput.addEventListener("change", updateCurrentText);
+//   elementTypeInput.addEventListener("change", updateCurrentText);
+
+//   updateCurrentText();
+
+//   // Ваш исходный скрипт
+//   const form = document.getElementById("updateForm");
+
+//   form.addEventListener("submit", function (e) {
+//     e.preventDefault();
+
+//     // const articleIndex = form.articleIndex.value;
+//     const articleIndex = form.articleIndex.value - 1;
+//     const elementType = form.elementType.value;
+//     const newText = form.newText.value;
+
+//     fetch("/update", {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//       body: JSON.stringify({ articleIndex, elementType, newText }),
+//     })
+//       .then((response) => response.json())
+//       .then((data) => {
+//         // Обработка ответа от сервера
+//       });
+//   });
+// });
